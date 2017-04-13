610f5c726fc738029e6b36e6512d8eb5e578217b
gargantophob <gargantophob@gmail.com>
2017-04-13 18:00:34 +0200

    minor updates

--- src/klondike/board/KlondikeBoard.java
+++ src/klondike/board/KlondikeBoard.java
@@ -1,7 +1,26 @@
 package klondike.board;
 
+import klondike.klondikeInterface.*;
+import klondike.model.KlondikePackFactory;
+
+/**
+ * Klondike board.
+ * @author xivanc03
+ */
 public class KlondikeBoard {
+	
 	public static void main(String argv[]) {
-		System.out.println("Hello World!");
+		
+		PackFactory factory = new KlondikePackFactory();
+		
+		Pack pack1 = factory.createDeck("C1UC2UC3U");
+		Pack pack2 = factory.createSourcePack("H0DHJDHQD");
+		System.out.println("1:\t" + pack1);
+		System.out.println("2:\t" + pack2);
+		
+		pack1.move(pack2, pack2.get(1));
+		System.out.println("---------------------------");
+		System.out.println("1:\t" + pack1);
+		System.out.println("2:\t" + pack2);
 	}
 }
