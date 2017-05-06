bbcc44c61ffd61927012c39ebe9ffb619f7f1ad7
Martin Ivančo <Martin@MacBook-Pro-uzivatela-Martin.local>
2017-05-06 11:02:19 +0200

    Structure update. Need to run bash lib/get-libs.sh before ant compile and ant run.

---
+++ lib/get-libs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# IJA Project
+# get-libs.sh
+# author: xivanc03
+
+# get zip of images
+wget http://www.stud.fit.vutbr.cz/~xivanc03/images.zip
+# unzip it
+unzip images.zip
+# clean it
+rm images.zip
